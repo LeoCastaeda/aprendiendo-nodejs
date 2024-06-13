1660dbb94f70f7ef361afb1a2320788e8594a351
Leo Castañeda <leocc_78@hotmail.com>
2024-06-13 21:44:19 +0200

    Segundo commit

---
+++ 00_HolaMundo/02_Modulos/CommonJS/app.js
@@ -0,0 +1,9 @@
+const calculadora = require("./calculadora.js");
+   c = console.log;
+
+   c("El resultado de Sumar 2 y 4 es igual a ", calculadora.sumar(2, 4));
+   c("El resultado de Restar 2 y 4 es igual a ", calculadora.restar(2, 4));
+   c("El resultado de Multiplicar 2 y 4 es igual a ", calculadora.multiplicar(2, 4));
+   c("El resultado de Dividir 4 y 2 es igual a ",calculadora.dividir(4, 2));
+   c("El resultado del Modulo 4 y 2 es igual a  ", calculadora.modulo(4, 2));
+
